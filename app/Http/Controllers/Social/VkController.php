2a8fb9f1d74ad26f9d2e151f132ba137a91f3bb6
Artem Zinoviev <arzinoviev@gmail.com>
2017-03-02 18:56:55 +0300

    user can login with own data and social/ authenticated user can link and unlink vk

--- app/Http/Controllers/Social/VkController.php
+++ app/Http/Controllers/Social/VkController.php
@@ -10,6 +10,11 @@ use Socialite;
 
 class VkController extends Controller
 {
+    public function login()
+    {
+        return Socialite::with('vkontakte')->scopes(['email'])->redirect();
+    }
+
     public function oauth2()
     {        
         return Socialite::with('vkontakte')
@@ -37,18 +42,36 @@ class VkController extends Controller
 
     public function redirect()
     {
-        try {
-            $vk_token = Socialite::with('vkontakte')->user()->token; // get token
+        $user = Socialite::with('vkontakte')->user();
 
-            /* insert token into user record */
-            $user = User::where('id', '=', Auth::user()->id)->first();
-            $user->vk_token = $vk_token;
-            $user->save();
-        } catch(\Exception $e) {
-            redirect()->route('home'); // TODO: add error message
-        }
+        $authUser = $this->findOrCreateUser($user);
+        Auth::login($authUser, true);
 
         return redirect()->route('home');
+    }
+
+    public function findOrCreateUser($user)
+    {
+        $existingUser = User::where('email', '=', $user->email)->first();
+
+        if(Auth::user()) {
+            $authUser = User::where('id', '=', Auth::user()->id)->first();
+            $authUser->vk_token = $user->token;
+            $authUser->save();
+
+            return $authUser;
+        } else if ($existingUser) {
+            return $existingUser;
+        }        
+
+        if(!$user->email) return redirect('register');
+        return User::create([
+            'name'        => $user->name ?? '',
+            'username'    => $user->email,
+            'email'       => $user->email,
+            'password'    => bcrypt('secret'),
+            'vk_token'    => $user->token,
+        ]);
     }
 
     public function forget()
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::group(['namespace' => 'Social', 'prefix' => 'service'], function() {
+    Route::get('/lvk', 'VkController@login')->name('vk_login');
     Route::get('/avk', 'VkController@oauth2')->name('vk_authorize');
     Route::get('/fvk', 'VkController@forget')->name('vk_forget');
     Route::get('/rvk', 'VkController@redirect')->name('vk_redirect');
